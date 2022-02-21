38d68119e01070e88b7355a12634b57de108defd
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2022-02-21 10:52:32 -0300

    Finish example

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+// Password is any UTF-16 string
+string clearPassword = "My$upeR Secr37 _123✨";
+byte[] clearPasswordBytes = Encoding.Unicode.GetBytes(clearPassword);
+
+Console.WriteLine($"-- Password ---");
+Console.WriteLine($"Clear...: {clearPassword}");
+Console.WriteLine($"(Hex)...: {Convert.ToHexString(clearPasswordBytes)}");
+Console.WriteLine($"(Base64): {Convert.ToBase64String(clearPasswordBytes)}");
+Console.WriteLine();
+
+// Configuration
+
+// Hash: 512 random bits (32 bytes), any other lenght is ok
+int hashSizeInBytes = 512 / 8;
+// Salt: 128 random bits (16 bytes), any other lenght is ok
+int saltSizeInBytes = 128 / 8;
+// Hash will use NIST recommended PBKDF2 config: https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#pbkdf2
+// Rfc2898DeriveBytes default: 1k SHA1 iterations
+// Recommended + random number of iterations (not a recommendation, but add some chaos)
+int iterations = 310000 + RandomNumberGenerator.GetInt32(0, 10000);
+var hashAlgo = HashAlgorithmName.SHA256;
+
+// Use this to create you own salt
+// byte[] salt = RandomNumberGenerator.GetBytes(saltSizeInBytes);
+// var hashed = new Rfc2898DeriveBytes(clearPassword, salt, iterations, hashAlgo);
+
+// Create a hash with salt size, number of iterations and a HMAC algo
+var hashGenerator = new Rfc2898DeriveBytes(clearPassword, saltSizeInBytes, iterations, hashAlgo);
+
+// The salt
+byte[] saltBytes = hashGenerator.Salt;
+// The hash
+byte[] hashBytes = hashGenerator.GetBytes(hashSizeInBytes);
+// The HMAC algo
+string algo = hashGenerator.HashAlgorithm.Name ?? "";
+// The iteration count
+int iterationCount = hashGenerator.IterationCount;
+
+// The value to persist
+string passwordToPersist = $"{Convert.ToBase64String(saltBytes)}|{iterationCount}|{Convert.ToBase64String(hashBytes)}";
+
+Console.WriteLine($"-- Rfc2898DeriveBytes ---");
+Console.WriteLine($"Random Salt (Hex).....: {Convert.ToHexString(saltBytes)}");
+Console.WriteLine($"Random Salt (Base64)..: {Convert.ToBase64String(saltBytes)}");
+
+Console.WriteLine($"\nHashing Algorithm.....: {algo} ({iterationCount} iterations)");
+
+Console.WriteLine($"Hash (Hex)............: {Convert.ToHexString(hashBytes)}");
+Console.WriteLine($"Hash (Base64).........: {Convert.ToBase64String(hashBytes)}");
+
+Console.WriteLine($"\n-- Persistent password string --\n{passwordToPersist}");
+
+// Testing password
+
+/// Query the persistance and parse parts
+string[] storedHashParts = passwordToPersist.Split("|");
+byte[] storedSalt = Convert.FromBase64String(storedHashParts[0]);
+int storedIterationCount = Convert.ToInt32(storedHashParts[1]);
+string storedPasswordHash = storedHashParts[2];
+
+/// Generate hash to compare -- Incorrect case
+string typedPassword = "bad$robot";
+var testHashGenerator = new Rfc2898DeriveBytes(typedPassword, storedSalt, storedIterationCount, hashAlgo);
+byte[] testHashBytes = testHashGenerator.GetBytes(hashSizeInBytes);
+string testHash = Convert.ToBase64String(testHashBytes);
+
+Console.WriteLine($"\n-- Incorrect attempt to log in --");
+Console.WriteLine($"Typed...: {typedPassword}");
+Console.WriteLine($"Hashed..: {testHash}");
+Console.WriteLine($"The user is {(storedPasswordHash == testHash ? "" : "not ")}ok to log in.");
+
+/// Generate hash to compare -- Correct case
+typedPassword = clearPassword;
+testHashGenerator = new Rfc2898DeriveBytes(typedPassword, storedSalt, storedIterationCount, hashAlgo);
+testHashBytes = testHashGenerator.GetBytes(hashSizeInBytes);
+testHash = Convert.ToBase64String(testHashBytes);
+
+Console.WriteLine($"\n-- Correct attempt to log in --");
+Console.WriteLine($"Typed...: {typedPassword}");
+Console.WriteLine($"Hashed..: {testHash}");
+Console.WriteLine($"The user is {(storedPasswordHash == testHash ? "" : "not ")}ok to log in.");
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# argon2id-pwd-hashing
-Experiments in Argon2id password hashing
+# csharp-pwd-hashing
+Experiments in password hashing
+
+## Method
+
+The hashing algorithm choice is PBKDF2, with SHA256 (310~320k iterations). It's ([OWASP and NIST recommended](https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#pbkdf2)).
+
+The example uses [this implementation](https://docs.microsoft.com/pt-br/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-6.0).
+
+### Salted-hash format (to persist)
+
+`base64-salt|int-number-of-iterations|base64-hash`
+
+## Notable methods
+
+- `System.Text.Encoding.Unicode.GetBytes` converts string to byte array
+- `System.Convert.ToHexString` converts string to hex string
+- `System.Convert.ToBase64String` converts string to base64 string
+- `System.Convert.FromBase64String` converts base64 string to string
+- `System.Security.Cryptography.RandomNumberGenerator.GetInt32` generates random int
+- `System.Security.Cryptography.RandomNumberGenerator.GetBytes` generates random byte array
+- `System.Security.Cryptography.Rfc2898DeriveBytes` apply hash algorith with any number of iterations, and generates salt byte array and hash byte array
+
+## Output example
+
+```
+-- Password ---
+Clear...: My$upeR Secr37 _123✨
+(Hex)...: 4D00790024007500700065005200200053006500630072003300370020005F003100320033002827
+(Base64): TQB5ACQAdQBwAGUAUgAgAFMAZQBjAHIAMwA3ACAAXwAxADIAMwAoJw==
+
+-- Rfc2898DeriveBytes ---
+Random Salt (Hex).....: 858048E553BC58552B8F62944D37AE9A
+Random Salt (Base64)..: hYBI5VO8WFUrj2KUTTeumg==
+
+Hashing Algorithm.....: SHA256 (319676 iterations)
+Hash (Hex)............: 5295307497A02B48AEEB6D944AF3FCFB332821A1FFC5F71A47313C4297268A91422D6A430609AB1081E4BE11E5CB25E9896C5324E73F6C5E27A4590D2E0F5D41
+Hash (Base64).........: UpUwdJegK0iu622USvP8+zMoIaH/xfcaRzE8QpcmipFCLWpDBgmrEIHkvhHlyyXpiWxTJOc/bF4npFkNLg9dQQ==
+
+-- Persistent password string --
+hYBI5VO8WFUrj2KUTTeumg==|319676|UpUwdJegK0iu622USvP8+zMoIaH/xfcaRzE8QpcmipFCLWpDBgmrEIHkvhHlyyXpiWxTJOc/bF4npFkNLg9dQQ==
+
+-- Incorrect attempt to log in --
+Typed...: bad$robot
+Hashed..: DFkuAS6jhnb8C3f2B1GP0n6LY7tBGckF62nEjH8cU8iWJLFa8LZX0fV2kUI9vGsttQvMJk+uoSTjQppr17lw1g==
+The user is not ok to log in.
+
+-- Correct attempt to log in --
+Typed...: My$upeR Secr37 _123✨
+Hashed..: UpUwdJegK0iu622USvP8+zMoIaH/xfcaRzE8QpcmipFCLWpDBgmrEIHkvhHlyyXpiWxTJOc/bF4npFkNLg9dQQ==
+The user is ok to log in.
+```
